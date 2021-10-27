40d167c90e4e8b113ea8c01bdd287ac91cff5512
Dario Marzella <>
2021-10-27 20:58:36 +0200

    -

--- bot.py
+++ bot.py
@@ -93,7 +93,7 @@ async def on_message(message):
                 await voice_client.move_to(channel)
 
             player = guild.voice_client
-            player.play(FFmpegPCMAudio(source='.'+soundboard[quote]['mp3'], executable='./ffmpeg.mp3'))
+            player.play(FFmpegPCMAudio(source='.'+soundboard[quote]['mp3'], executable='./ffmpeg.exe'))
 
     if message.content == 'Barbacitazioni':
         response = 'Buongiorno! Grazie per avermi invitato. Oggi posso deliziarvi con queste citazioni:\n' + ('\n').join(list(soundboard.keys()))
